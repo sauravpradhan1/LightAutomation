9f48ae5148df1c41fa8ae16e0c2fa786faf7d83b
Saurav Pradhan <42857122+sauravpradhan1@users.noreply.github.com>
2016-11-14 03:15:54 +1000

    Fragment Display

---
+++ LightControl/app/src/main/java/com/lightcontrol/FragmentDisplay.java
@@ -0,0 +1,33 @@
+package com.lightcontrol;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Switch;
+
+
+public class FragmentDisplay extends Fragment {
+
+
+
+    public static Fragment newInstance(Context context) {
+        FragmentDisplay f = new FragmentDisplay();
+
+        return f;
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_display, container, false);
+    }
+
+}
